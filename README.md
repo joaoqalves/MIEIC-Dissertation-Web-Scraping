39c219859c34a03501aaa4d36700eb46a97456ea
João Alves <joaoqalves@gmail.com>
2013-03-06 20:56:38 +0000

    Working with the newest version of SIGARRA

---
+++ README.md
@@ -0,0 +1,8 @@
+## About ##
+This is a small scrapper for MIEIC Master's dissertations. The original version
+was written by [João Figueiredo](https://github.com/lucalanca/).
+I did this new version because SIGARRA was updated between the 1st and 2nd
+semester and his version do not work anymore.
+
+## Contribute ##
+Just fork it ;)
--- thesis-web-scrapping.rb
+++ thesis-web-scrapping.rb
@@ -1,30 +1,32 @@
-
+# encoding: UTF-8
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
+require 'set'
 
-STUDENT_ID = "080509065" # INSERT YOUR STUDENT ID HERE
+STUDENT_ID = "112374" # INSERT YOUR STUDENT ID HERE
+STUDENT_NUMBER =  "200801722" # INSERT YOUR STUDENT NUMBER HERE
 # INSERT YOUR COOKIE BELOW
-COOKIE = "GPAG_CCORRENTE_GERAL_CONTA_CORRENTE_VIEW=0; __utma=130543245.768358309.1337265203.1337265203.1337265203.1; __utmz=130543245.1337265203.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _pk_ref.5.7cdc=%5B%22%22%2C%22%22%2C1338834354%2C%22http%3A%2F%2Fupin.up.pt%2F%22%5D; _pk_id.5.7cdc=b0c5b8abeea6c398.1337209994.2.1338834354.1337210096.; _pk_ref.23.7cdc=%5B%22%22%2C%22%22%2C1338834354%2C%22http%3A%2F%2Fupin.up.pt%2F%22%5D; _pk_id.23.7cdc=b0c5b8abeea6c398.1337209994.2.1338834354.1337210096.; __atuvc=3|20,0|21,0|22,1|23; FEUPSI_SESSION=21027835; FEUPSI_SECURITY=G9p/3ieAGV9EYOj; FEUPHTTP_SESSION=458510911"
+COOKIE = "FCNAUPHTTP_SESSION=10139674; FCUPHTTP_SESSION=29626930; FDUPHTTP_SESSION=13588674; FEPHTTP_SESSION=47986428; _pk_ref.5.7cdc (...)"
 
-BASE_URL = "https://sigarra.up.pt/feup/"
-INDEX_URL = "#{BASE_URL}ESTAGIOS_ALUNOS.LISTA_EMPRESAS?p_aluno=#{STUDENT_ID}&p_processo=1103"
-PROFESSOR_URL = "#{BASE_URL}estagios_alunos.lista_propostas?p_aluno=#{STUDENT_ID}&p_processo=1103&p_doc_codigo="
-COMPANY_URL = "#{BASE_URL}estagios_alunos.lista_propostas?p_aluno=#{STUDENT_ID}&p_processo=1103&p_inst_codigo="
-DISSERTATION_URL = "#{BASE_URL}estagios_empresas.ver_dados_proposta?p_id="
+BASE_URL = "https://sigarra.up.pt/feup/pt/"
+PROCESS_NUMBER = "18232"
+INDEX_URL = "#{BASE_URL}estagios_empresas.lista_propostas_processo?p_aluno_id=#{STUDENT_ID}&p_processo=#{PROCESS_NUMBER}&pv_perfil=ALU"
+PROFESSOR_URL = "#{BASE_URL}estagios_alunos.lista_propostas?p_aluno=#{STUDENT_NUMBER}&p_processo=#{PROCESS_NUMBER}&pv_perfil=ALU&p_doc_codigo="
+DISSERTATION_URL = "#{BASE_URL}estagios_empresas.ver_dados_proposta?pv_perfil=ALU&p_id="
 
-PROFESSORS_REGEX = "p_doc_codigo"
-COMPANIES_REGEX = "p_inst_codigo"
-THESIS_REGEX = "estagios_empresas"
+PROFESSORS_REGEX = "func_geral.formview?p_codigo"
+COMPANIES_REGEX = "pct_user"
+THESIS_REGEX = "estagios_empresas.ver_dados_proposta"
 
 def get_ids_in_link_by_href_regex(doc, link_regex)
-	ids = []
+	ids = Set.new
 	doc.xpath('//a[@href]').each do |link|
-		if link['href'] =~ /#{link_regex}/
-			ids << link['href'].split("=").last
-		end
+	  if link['href'] =~ /#{Regexp.quote(link_regex)}/
+		  ids.add link['href'].split("=")[1].split("&").first
+	  end
 	end
-	ids
+	ids.to_a
 end
 
 def get_html(url)
@@ -37,26 +39,30 @@ def get_thesis_html_pages()
 	index_page = get_html(INDEX_URL)
 	professors_ids = get_ids_in_link_by_href_regex(index_page, PROFESSORS_REGEX)
 	companies_ids = get_ids_in_link_by_href_regex(index_page, COMPANIES_REGEX)
-
+	
 	professors_ids.each do |p_id|
-		proposed_dissertation_page = get_html(PROFESSOR_URL+p_id)
-		dissertation_ids += get_ids_in_link_by_href_regex(proposed_dissertation_page, THESIS_REGEX)
+	  proposed_dissertation_page = get_html(PROFESSOR_URL+p_id)
+	  dissertation_ids += get_ids_in_link_by_href_regex(proposed_dissertation_page, THESIS_REGEX)
 	end
-	# companies_ids.each do |p_id|
-	# 	proposed_dissertation_page = get_html(COMPANY_URL+p_id)
-	# 	dissertation_ids += get_ids_in_link_by_href_regex(proposed_dissertation_page, THESIS_REGEX)
-	# end
+	
+	companies_ids.each do |pct|
+	  proposed_dissertation_page = get_html(PROFESSOR_URL+pct)
+	  dissertation_ids += get_ids_in_link_by_href_regex(proposed_dissertation_page, THESIS_REGEX)
+	end
+	
 	dissertation_ids.each do |thesis|
-		dissertation_pages << get_html(DISSERTATION_URL+thesis)
+	  dissertation_pages << get_html(DISSERTATION_URL+thesis)
 	end
+	
 	dissertation_pages
+	
 end
 
 def dissertations_pages_to_array_of_hashes(dissertations_pages)
 	all_dissertations = Array.new
 	dissertations_pages.each do |dis|
 		dissertation = Hash.new
-		dis.css('td.conteudocentral > table tr').each do |tab|
+		dis.css('#conteudo > table tr').each do |tab|
 			key = nil
 			value = nil
 			tab.css('td').each_with_index do |t,index|
